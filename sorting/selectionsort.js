a3074bdb21cae539f09fb55a149a48b9e4e035ba
niranjandascp <niranjandas.dev@gmail.com>
2025-11-04 15:04:39 +0530

    parctice: selectionsort

---
+++ sorting/selectionsort.js
@@ -0,0 +1,13 @@
+// let arr = [5, 3, 8, 4, 2];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//   let minIndex = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] < arr[minIndex]) {
+//       minIndex = j;
+//     }
+//   }
+//   [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+// }
+// console.log(arr);
+
