f9d66da2d3753deb8a469bffe8c9b843b74f482c
niranjandascp <niranjandas.dev@gmail.com>
2025-11-04 12:25:31 +0530

    test:bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,11 +1,22 @@
-let arr = [5, 3, 8, 4, 2];
+// let arr = [5, 3, 8, 4, 2];
 
-for (let i = 0; i < arr.length - 1; i++) {
+// for (let i = 0; i < arr.length - 1; i++) {
+//   for (let j = 0; j < arr.length - 1 - i; j++) {
+//     if (arr[j] > arr[j + 1]) {
+//       [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//     }
+//   }
+// }
+// console.log(arr); 
+
+
+let arr = [5,3,8,4,2]
+
+for (let i = 0 ; i < arr.length - 1; i++) {
   for (let j = 0; j < arr.length - 1 - i; j++) {
     if (arr[j] > arr[j + 1]) {
-      // swap
-      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      [arr[j], arr[j+1]] = [arr[j+1],arr[j]]
     }
   }
 }
-console.log(arr); // [2, 3, 4, 5, 8]
+console.log(arr)
