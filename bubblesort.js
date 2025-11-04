5b1ca173cac34a3583d5ab47867a948258ab9010
niranjandascp <niranjandas.dev@gmail.com>
2025-11-04 13:56:05 +0530

    test1

--- bubblesort.js
+++ bubblesort.js
@@ -7,16 +7,7 @@
 //     }
 //   }
 // }
-// console.log(arr); 
+// console.log(arr);
 
+// let arr = [5,3,8,4,2]
 
-let arr = [5,3,8,4,2]
-
-for (let i = 0 ; i < arr.length - 1; i++) {
-  for (let j = 0; j < arr.length - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      [arr[j], arr[j+1]] = [arr[j+1],arr[j]]
-    }
-  }
-}
-console.log(arr)
