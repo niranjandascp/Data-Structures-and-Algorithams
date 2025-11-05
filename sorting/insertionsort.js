0aa14076da383ea780ff655b8fc1e74e7d2ac6eb
niranjandascp <niranjandas.dev@gmail.com>
2025-11-05 15:22:20 +0530

    practice:insertionsort

---
+++ sorting/insertionsort.js
@@ -0,0 +1,14 @@
+let arr = [5, 3, 8, 4, 2]
+
+const n = arr.length
+for (let i = 1; i < n; i++) {
+    let current = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j]
+        j--
+    }
+    arr[j + 1] = current
+}
+console.log(arr)
