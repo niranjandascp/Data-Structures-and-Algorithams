dae5559add3cd6355f13506e368531c1fcb24f63
niranjandascp <niranjandas.dev@gmail.com>
2025-11-13 11:36:35 +0530

    practice

--- array/removeduplicates.js
+++ array/removeduplicates.js
@@ -1,5 +1,5 @@
-// let arr = [1, 2, 3, 2 , 4, 1, 5, 6 , 3]
+let arr = [1, 2, 3, 2 , 4, 1, 5, 6 , 3]
 
-// let uniqeArr = [...new Set(arr)]
-// console.log("orginal arr:",arr)
-// console.log("remove duplicates:", uniqeArr)
+let uniqeArr = [...new Set(arr)]
+console.log("orginal arr:",arr)
+console.log("remove duplicates:", uniqeArr)
